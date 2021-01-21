792be568ce8546117fb92975b77565c7724d0607
Hazel-imuetz <75885135+Hazel-imuetz@users.noreply.github.com>
2021-01-21 10:32:15 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,7 +1,7 @@
 let handler = async m => m.reply(`
 ╭─「 Donasi • Pulsa 」
-│ • Indosat Ooredoo [081515860089]
-│ • Telkomsel [081334177340]
+│ • Tri [+628987183535]
+│ • By.u [+6285157756287]
 ╰────
 `.trim()) // Tambah sendiri kalo mau
 handler.help = ['donasi']
