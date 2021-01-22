18e216eb90a5066a6b9bb95adde7f1abf5f67dff
Hazel-imuetz <75885135+Hazel-imuetz@users.noreply.github.com>
2021-01-22 17:46:34 +0700

    Update apakah.js

--- plugins/apakah.js
+++ plugins/apakah.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn, text }) => {
   conn.reply(m.chat, `
 *Pertanyaan:* ${m.text}
-*Jawaban:* ${pickRandom(['Ya','Mungkin iya','Mungkin','Mungkin tidak','Tidak','Tidak mungkin'])}
+*Jawaban:* ${pickRandom(['Iya','Mungkin iya','Mungkin','Mungkin tidak','Tidak','Tidak mungkin','Apa Mangsud Nanya Kek Gitu','True'])}
 `.trim(), m)
 }
 handler.help = ['apakah <teks>?']
